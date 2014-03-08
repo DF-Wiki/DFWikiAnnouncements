231b4e6640cd442a686501ea270b2c85c084613a
Lethosor <lethosor@users.noreply.github.com>
2014-03-07 22:41:51 -0500

    Create Announcements.js
    Placeholder

---
+++ Announcements.js
@@ -0,0 +1,15 @@
+// DF Wiki Announcements
+
+Annc = (function(){
+	var Annc = {}, prefix = 'dfwk-annc-';
+	Annc.load = function() {
+		
+	};
+	Annc.save = function(anncs) {
+		
+	};
+	Annc.status = function() {
+	
+	};
+	return Annc;
+})();
